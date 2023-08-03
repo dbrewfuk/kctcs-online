b9cdb72c96b9e78ec0a42358100dec4fa4c81d13
dbrewfuk <dbrewfukuhara@gmail.com>
2023-08-03 18:48:34 -0400

    Another commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-<<<<<<< HEAD
 import "./App.css";
-=======
->>>>>>> 293d051 (Another commit.)
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-<<<<<<< HEAD
       <div>
         <h1>App</h1>
     </div>
