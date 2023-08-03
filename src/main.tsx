8ae1534f17379f91c9260d601e46764de6fdd89a
dbrewfuk <dbrewfukuhara@gmail.com>
2023-08-03 19:28:15 -0400

    Another commit.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-<<<<<<< HEAD
-import "./index.css";
-=======
 import "./index.scss";
->>>>>>> 293d051 (Another commit.)
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
